054c14ea6c39ce2dfab51ece2f91fa44f58d9f72
Shakespeare89 <growingupcollegestyle@gmail.com>
2020-09-21 18:46:17 -0600

    first commit

---
+++ src/components/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import PizzaApp from './OrderForm';
+
+export const Order = () => (
+  <div className="order-page">
+    <h2>Contact</h2>
+  
+    <PizzaApp />
+
+    </div>
+ 
+)
